10ff4e86ccbde6fa1839232ae19e9fc52fc37ceb
mikusaa <mikusaaaaa@gmail.com>
2024-04-07 21:52:30 +0800

    简单复制粘贴兼容了php8，修改了下版本号

--- Plugin.php
+++ Plugin.php
@@ -1,16 +1,27 @@
 <?php
 
-if (!defined('__TYPECHO_ROOT_DIR__')) exit;
+namespace TypechoPlugin\Copyright;
+
+use Typecho\Plugin\PluginInterface;
+use Typecho\Plugin\Exception as PluginException;
+use Typecho\Widget\Helper\Form;
+use Typecho\Widget\Helper\Form\Element\Text;
+use Widget\Options;
+use Utils\Helper; 
+
+if (!defined('__TYPECHO_ROOT_DIR__')) {
+    exit;
+}
 /**
- * Copyright for Typecho 修改版
+ * Copyright for Typecho
  *
  * @package Copyright
  * @author  mikusa
- * @version 1.0.6
+ * @version 2.0.0
  * @link https://github.com/mikusaa/Copyright-for-Typecho
  */
 
-class Copyright_Plugin implements Typecho_Plugin_Interface {
+ class Plugin implements PluginInterface {
     /**
      * 激活插件方法,如果激活失败,直接抛出异常
      *
@@ -19,7 +30,7 @@ class Copyright_Plugin implements Typecho_Plugin_Interface {
      * @throws Typecho_Plugin_Exception
      */
     public static function activate() {
-        Typecho_Plugin::factory('Widget_Abstract_Contents')->contentEx = array('Copyright_Plugin', 'Copyright');
+        \Typecho\Plugin::factory('Widget_Abstract_Contents')->contentEx = array('Copyright_Plugin', 'Copyright');
     }
 
     /**
@@ -37,10 +48,10 @@ class Copyright_Plugin implements Typecho_Plugin_Interface {
      * 获取插件配置面板
      *
      * @access public
-     * @param Typecho_Widget_Helper_Form $form 配置面板
+     * @param Form $form 配置面板
      * @return void
      */
-    public static function config(Typecho_Widget_Helper_Form $form) {
+    public static function config(Form $form): void {
         echo '<p>欢迎使用 Typecho 版权插件，你正在使用的是增加了“<strong>封面来源</strong>”的修改版。（还有一些颜色上的修改）</p>';
         echo '<p>此插件帮助你设置文章与独立页面的版权声明，它会附在内容末尾。你也可以对特定某篇内容设置版权信息。</p>';
         echo '<p>版权信息借助插件与 Typecho 的自定义字段功能实现，只与插件或特定内容关联，而不会修改其内容本身，也不会在数据库中与文本混同。</p>';
@@ -51,26 +62,32 @@ class Copyright_Plugin implements Typecho_Plugin_Interface {
         echo '<hr />';
         echo '作者曾有很长一段时间没有维护此插件，这期间它随 Typecho 1.1 更新而失效。感谢该网友修复了此插件，使得它可以在 Typecho 1.1 下继续工作：<a href="https://lolico.moe" target="_blank">神代綺凜</a>';
         echo '<hr />';
-        $author = new Typecho_Widget_Helper_Form_Element_Text('author', NULL, _t('作者名称'), _t('作者'));
+
+        $author = new Text('author', NULL, _t('作者名称'), _t('作者'));
         $form->addInput($author);
-        $notice = new Typecho_Widget_Helper_Form_Element_Text('notice', NULL, _t('转载时须注明出处及本声明'), _t('声明'));
+
+        $notice = new Text('notice', NULL, _t('转载时须注明出处及本声明'), _t('声明'));
         $form->addInput($notice);
-        $showURL = new Typecho_Widget_Helper_Form_Element_Checkbox('showURL', array(1 => _t('显示原（本）文链接')), NULL, NULL, NULL);
+
+        $showURL = new Form\Element\Radio('showURL', array('1' => _t('启用'),'0' => _t('不启用'),), '0', _t('显示原（本）文链接'));
         $form->addInput($showURL);
-        $showOnPost = new Typecho_Widget_Helper_Form_Element_Checkbox('showOnPost', array(1 => _t('在文章显示')), NULL, NULL, NULL);
+
+        $showOnPost = new Form\Element\Radio('showOnPost', array('1' => _t('启用'),'0' => _t('不启用'),), '0', _t('在文章页显示'));
         $form->addInput($showOnPost);
-        $showOnPage = new Typecho_Widget_Helper_Form_Element_Checkbox('showOnPage', array(1 => _t('在独立页面显示')), NULL, NULL, NULL);
+
+        $showOnPage = new Form\Element\Radio('showOnPage', array('1' => _t('启用'),'0' => _t('不启用'),), '0', _t('在独立页面显示'));
         $form->addInput($showOnPage);
+
     }
 
     /**
      * 个人用户的配置面板
      *
      * @access public
-     * @param Typecho_Widget_Helper_Form $form
+     * @param Form $form
      * @return void
      */
-    public static function personalConfig(Typecho_Widget_Helper_Form $form) {
+    public static function personalConfig(Form $form) {
     }
 
     /**
@@ -90,13 +107,13 @@ class Copyright_Plugin implements Typecho_Plugin_Interface {
 
     private static function globalCopyright($widget) {
         $cr = array('show_on_post' => '', 'show_on_page' => '', 'show_url' => '', 'author' => '', 'url' => '', 'notice' => '', 'cover' => '');
-        $cr['show_on_post'] = Typecho_Widget::widget('Widget_Options')->plugin('Copyright')->showOnPost;
-        $cr['show_on_page'] = Typecho_Widget::widget('Widget_Options')->plugin('Copyright')->showOnPage;
-        $cr['show_url'] = Typecho_Widget::widget('Widget_Options')->plugin('Copyright')->showURL[0];
-        $cr['author'] = Typecho_Widget::widget('Widget_Options')->plugin('Copyright')->author;
-        $cr['url'] = Typecho_Widget::widget('Widget_Options')->plugin('Copyright')->url;
-        $cr['notice'] = Typecho_Widget::widget('Widget_Options')->plugin('Copyright')->notice;
-        $cr['cover'] = Typecho_Widget::widget('Widget_Options')->plugin('Copyright')->cover;
+        $cr['show_on_post'] = \Typecho\Widget::widget('Widget_Options')->plugin('Copyright')->showOnPost;
+        $cr['show_on_page'] = \Typecho\Widget::widget('Widget_Options')->plugin('Copyright')->showOnPage;
+        $cr['show_url'] = \Typecho\Widget::widget('Widget_Options')->plugin('Copyright')->showURL[0];
+        $cr['author'] = \Typecho\Widget::widget('Widget_Options')->plugin('Copyright')->author;
+        $cr['url'] = \Typecho\Widget::widget('Widget_Options')->plugin('Copyright')->url;
+        $cr['notice'] = \Typecho\Widget::widget('Widget_Options')->plugin('Copyright')->notice;
+        $cr['cover'] = \Typecho\Widget::widget('Widget_Options')->plugin('Copyright')->cover;
         return $cr;
     }
 
@@ -148,7 +165,7 @@ class Copyright_Plugin implements Typecho_Plugin_Interface {
         $t_url = '';
         if ($cr['is_enable']) {
             if ($cr['author']) {
-                $parsedAuthor = Typecho_Widget::widget('Widget_Abstract_Contents')->markdown($cr['author']);
+                $parsedAuthor = \Typecho\Widget::widget('Widget_Abstract_Contents')->markdown($cr['author']);
                 $parsedAuthor = strip_tags($parsedAuthor, '<a><em><strong>');  // 保留链接和强调标签
                 $t_author = '<p class="content-copyright"><strong>本文作者：</strong>' . $parsedAuthor . '</p>';
             }
@@ -160,7 +177,7 @@ class Copyright_Plugin implements Typecho_Plugin_Interface {
                 }
             }
             if ($cr['cover']) {
-                $parsedCover = Typecho_Widget::widget('Widget_Abstract_Contents')->markdown($cr['cover']);
+                $parsedCover = \Typecho\Widget::widget('Widget_Abstract_Contents')->markdown($cr['cover']);
                 $parsedCover = strip_tags($parsedCover, '<a><em><strong>');  // 保留链接和强调标签
                 $t_cover = '<p class="content-copyright"><strong>封面出处：</strong>' . $parsedCover . '</p>';
             }
